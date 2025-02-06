dd2b55eecca31007784abb21613f5106d4fd5f4f
Clara Jordan <czmj@pop-os.localdomain>
2025-02-06 21:42:10 +0000

    Remove sold, exhibitions

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,6 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  // const blogPost = path.resolve(`./src/templates/blog-post.js`)
   return graphql(
     `
     {
@@ -30,49 +29,7 @@ exports.createPages = ({ graphql, actions }) => {
     if (result.errors) {
       throw result.errors
     }
-
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
-    // Template For blog-post
-    const blogPost = posts.filter(item => item.node.frontmatter.templateKey === 'blog-post')
-    blogPost.forEach((post, index) => {
-      const previous = index === blogPost.length - 1 ? null : blogPost[index + 1].node
-      const next = index === 0 ? null : blogPost[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' : `/${post.node.fields.slug.split('/').slice(2, -1).join('/')}`,
-        component: path.resolve(
-          `src/templates/blog-post.js`
-        ),
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-    // Template For exhibitions-sub-page
-    const exhibitionsPage = posts.filter(item => item.node.frontmatter.templateKey === 'exhibitions-sub-page')
-    exhibitionsPage.forEach((post, index) => {
-      const previous = index === exhibitionsPage.length - 1 ? null : exhibitionsPage[index + 1].node
-      const next = index === 0 ? null : exhibitionsPage[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' : `/${post.node.fields.slug.split('/').slice(2, -1).join('/')}`,
-        component: path.resolve(
-          `src/templates/exhibitions-sub-page.js`
-        ),
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-    //   Template For exhibitions-sub-page
-    const allPage = posts.filter(item =>
-      item.node.frontmatter.templateKey !== 'blog-post' &&
-      item.node.frontmatter.templateKey !== 'exhibitions-sub-page')
+    const allPage = result.data.allMarkdownRemark.edges
     allPage.forEach((post, index) => {
       const previous = index === allPage.length - 1 ? null : allPage[index + 1].node
       const next = index === 0 ? null : allPage[index - 1].node
@@ -84,8 +41,6 @@ exports.createPages = ({ graphql, actions }) => {
         ),
         context: {
           slug: post.node.fields.slug,
-          // previous,
-          // next,
         },
       })
     })
